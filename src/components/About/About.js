d65ec8c74dde808ae85908aea1ab59e1a4b306e5
Katarzyna Kęsy <kasia.kesy@gmail.com>
2019-08-12 00:21:17 +0200

    structure & styles

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,12 +1,26 @@
-import React, { Component } from 'react';
-import './About.scss';
+import React from "react";
+import "./About.scss";
 
-export default class About extends Component {
-  render() {
-    return (
-      <section className="about">
+function About() {
+  return (
+    <section className="about">
+      <div className="descriptionBox">
         <h2>O nas</h2>
-      </section>
-    )
-  }
+        <img className="descriptionBox__ornament" src="/images/ornament.png" alt="ornament" />
+        <p>
+          Nori grape silver beet broccoli kombu beet greens fava bean potato
+          quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil
+          turnip greens parnsnip
+        </p>
+        <div className="descriptionBox__signature">
+          <img src="/images/signature.png" alt="podpis" />
+        </div>
+      </div>
+      <div className="imageBox">
+        <img src="/images/about.png" alt=""/>
+      </div>
+    </section>
+  );
 }
+
+export default About;
