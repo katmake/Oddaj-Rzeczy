38cd22f175eac7abae92e6f7035aee741a75106a
Katarzyna Kęsy <kasia.kesy@gmail.com>
2019-08-12 19:03:22 +0200

    structure & styles

--- src/components/CallToAction/CallToAction.js
+++ src/components/CallToAction/CallToAction.js
@@ -1,13 +1,25 @@
-import React, { Component } from 'react';
-import './CallToAction.scss';
+import React from "react";
+import steps from "./steps";
+import Step from "../Step/Step";
+import "./CallToAction.scss";
 
-export default class CallToAction extends Component {
-
-  render() {
-    return (
-      <section className="callToAction">
-        <h2>Wystarczą 4 proste kroki</h2>
-      </section>
-    )
-  }
+function CallToAction() {
+  return (
+    <section className="callToAction">
+      <h2>Wystarczą 4 proste kroki</h2>
+      <img
+        className="callToAction__ornament"
+        src="/images/ornament.png"
+        alt="ornament"
+      />
+      <div className="callToAction__stepsBox">
+        {Object.keys(steps).map(key => (
+          <Step key={key} step={steps[key]} />
+        ))}
+      </div>
+      <button>Załóż konto</button>
+    </section>
+  );
 }
+
+export default CallToAction;
