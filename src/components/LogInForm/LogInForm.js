7e2f27ab679637e5c39e9689c7e1ab3482a11222
Katarzyna Kęsy <kasia.kesy@gmail.com>
2019-08-14 00:54:11 +0200

    structure, content, styles, react-scroll

---
+++ src/components/LogInForm/LogInForm.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const LogInForm = () => {
+  return (
+    <>
+      <h1>LogInForm</h1>
+      <a href="./rejestracja">Załóż konto</a>
+    </>
+  );
+};
+
+export default LogInForm;
--- src/components/Router/Router.js
+++ src/components/Router/Router.js
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import LandingPage from "../LandingPage";
 import SignUpForm from "../SignUpForm";
+import LogInForm from "../LogInForm"
 
 const Router = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={LandingPage}/>
-      <Route exact path="/logowanie" component={SignUpForm}/>
+      <Route exact path="/rejestracja" component={SignUpForm}/>
+      <Route exact path="/logowanie" component={LogInForm}/>
     </Switch>
   </BrowserRouter>
 );
