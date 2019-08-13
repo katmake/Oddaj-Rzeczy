04bd1e6713b91b5c69e7e56bc42a402b6cde8636
Katarzyna Kęsy <kasia.kesy@gmail.com>
2019-08-13 21:31:15 +0200

    class component Achievements replaced by functional component

--- src/components/Achievements/Achievements.js
+++ src/components/Achievements/Achievements.js
@@ -1,18 +1,18 @@
-import React, { Component } from "react";
+import React from "react";
 import accomplishments from "./accomplishments";
 import Achievement from "../Achievement";
 import "./Achievements.scss";
 
-export default class Achievements extends Component {
-  render() {
-    return (
-      <section id="achievements" className="achievements">
-        <div className="achievementsBox">
-          {Object.keys(accomplishments).map(key => (
-            <Achievement key={key} accomplishment={accomplishments[key]} />
-          ))}
-        </div>
-      </section>
-    );
-  }
-}
+const Achievements = () => {
+  return (
+    <section id="achievements" className="achievements">
+      <div className="achievementsBox">
+        {Object.keys(accomplishments).map(key => (
+          <Achievement key={key} accomplishment={accomplishments[key]} />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Achievements;
