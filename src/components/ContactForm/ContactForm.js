7ebe5b7f53288b67cce24c4c99d71194adf71d78
Katarzyna Kęsy <kasia.kesy@gmail.com>
2019-08-27 18:55:32 +0200

    controlled form, fetch

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -3,9 +3,42 @@ import "./ContactForm.scss";
 
 
 export default class ContactForm extends Component {
+  state = {
+    name: "",
+    email: "",
+    msg: ""
+  }
+
+  handleOnChange = e => {
+    e.preventDefault();
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
 
   handleSubmit = (e) => {
     e.preventDefault();
+
+    let name = this.state.name;
+    let email = this.state.email;
+    let msg = this.state.msg;
+
+    fetch("https://fer-api.coderslab.pl/v1/portfolio/contact", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({name, email, msg})
+    })
+    .then(function(response) {
+      return response.text();
+    })
+    .then(function(text) {
+      console.log(text);
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
   }
 
   render() {
@@ -13,11 +46,11 @@ export default class ContactForm extends Component {
       <form className="contactForm" onSubmit={this.handleSubmit}>
       <h3 className="contactForm__title">Formularz Kontaktowy</h3>
       <div className="contactForm__contactData">
-        <input type="text" placeholder="Imię"/>
-        <input type="email" placeholder="Email"/>
+        <input type="text" placeholder="Imię" name="name" value={this.state.name} onChange={this.handleOnChange}/>
+        <input type="email" placeholder="Email" name="email" value={this.state.email} onChange={this.handleOnChange}/>
       </div>
 
-      <textarea className="contactForm__contactMsg" name="" id="" rows="3" placeholder="Wiadomość"/>
+      <textarea className="contactForm__contactMsg" rows="3" placeholder="Wiadomość" name="msg" value={this.state.msg} onChange={this.handleOnChange}/>
       <button type="submit" className="contactForm__btn">Wyślij</button>
     </form>
     )
